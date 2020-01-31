655952a0b62c2aad767e58266f356cdf6c892863
金山智昭 <kanayamatomoaki@kanayamatomoakinoiMac-2.local>
2020-01-31 10:31:26 +0900

    title size

--- src/component/AppBar.js
+++ src/component/AppBar.js
@@ -2,24 +2,32 @@ import React from 'react';
 import { AppBar, Toolbar, Typography, Tab, Tabs } from '@material-ui/core';
 import MapIcon from '@material-ui/icons/Explore';
 import ChartIcon from '@material-ui/icons/BarChart';
+import { SituationContext } from '../Provider';
 
 function AppToolbar (props){
-  const { tabIndex, onChangeTabIndex, displaySize } = props;
+  const { tabIndex, onChangeTabIndex } = props;
   const handleChange = (ev, newValue) => {
     onChangeTabIndex(newValue);
   }
-  const titleSize = displaySize === 'desktop' ? 'h4' : 'h6';
-  return <AppBar position="fixed">
-  <Toolbar>
-    <Typography variant={titleSize}>
-      SITUATION of Coronavirus (2019-nCoV)
-    </Typography>
-  </Toolbar>
-    <Tabs value={tabIndex} onChange={handleChange} aria-label="simple tabs example">
-      <Tab icon={<MapIcon />} label="Map" />
-      <Tab icon={<ChartIcon />} label="Chart" />
-    </Tabs>
-  </AppBar>
+  return (<SituationContext.Consumer>
+    {
+      ({displaySize}) => {
+      const titleSize = displaySize === 'desktop' ? 'h4' : 'body1';
+      return <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant={titleSize}>
+            SITUATION of Coronavirus (2019-nCoV)
+          </Typography>
+        </Toolbar>
+          <Tabs value={tabIndex} onChange={handleChange} aria-label="simple tabs example">
+            <Tab icon={<MapIcon />} label="Map" />
+            <Tab icon={<ChartIcon />} label="Chart" />
+          </Tabs>
+        </AppBar>
+    }
+  }
+  </SituationContext.Consumer>);
+
 }
 
 export default AppToolbar;
